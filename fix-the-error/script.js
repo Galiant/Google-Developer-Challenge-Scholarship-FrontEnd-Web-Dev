96280d6d66151dfb39de1b7b8bbd130f36fb78e7
Galiant <antonijodev@gmail.com>
2017-11-26 13:20:52 +0000

    Fix the error

--- fix-the-error/script.js
+++ fix-the-error/script.js
@@ -8,9 +8,19 @@ Here is a for loop that's supposed to print the numbers 5 through 9. Fix the err
       console.log(x);
     }
 
+The for loop below has an error. Fix it!
+
+    for (var k = 0 k < 200 k++) {
+      console.log(k);
+    }
  */
 
 // fix the for loop
 for (var x = 5; x < 10; x++) {
   console.log(x);
+}
+
+// fix the for loop
+for (var k = 0; k < 200; k++) {
+  console.log(k);
 }
