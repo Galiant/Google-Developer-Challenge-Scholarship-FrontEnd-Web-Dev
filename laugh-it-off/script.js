7237131417419b7858a8b98dcc7e02f47494a0af
Galiant <antonijodev@gmail.com>
2017-11-28 12:52:43 +0000

    Laugh it off

--- laugh-it-off/script.js
+++ laugh-it-off/script.js
@@ -13,3 +13,32 @@ function laugh() {
 }
 
 console.log(laugh());
+
+/*
+ * Programming Quiz: Laugh it Off 2 (5-2)
+ *
+ * 
+Directions:
+Write a function called laugh() that takes one parameter, num that represents the number of "ha"s to return.
+
+  TIP: You might need a loop to solve this!
+
+Here's an example of the output and how to call the function that you will write:
+
+    console.log(laugh(3));
+    Prints: "hahaha!"
+
+ * Note:
+ *  - make sure your the final character is an exclamation mark ("!")
+ */
+
+function laugh(num) {
+  var laughNumber = "";
+  var text = "ha";
+  for( var x = 0; x < num; x++) {
+      laughNumber += text;
+  }
+  return laughNumber + "!";
+}
+
+console.log(laugh(3));
