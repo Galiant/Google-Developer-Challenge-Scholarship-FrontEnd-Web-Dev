92f33327709796aec9ec6fee05bec2943236aff1
Galiant <antonijodev@gmail.com>
2017-12-01 12:28:46 +0000

    The Price is Right

---
+++ the-price-is-right/script.js
@@ -0,0 +1,23 @@
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ * 
+Directions:
+Starting with this array of prices, change the prices of the 1st, 3rd, and 7th elements in the array.
+
+    var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+  TIP: The 1st element of any array has an index of 0.
+
+Afterwards, print out the prices array to the console.
+
+ */
+
+var prices = [1.23, 48.11, 90.11, 8.5, 9.99, 1.0, 1.1, 67.0];
+
+// your code goes here
+
+prices[0] = 1.13;
+prices[2] = 89.11;
+prices[6] = 1.0;
+
+console.log(prices);
