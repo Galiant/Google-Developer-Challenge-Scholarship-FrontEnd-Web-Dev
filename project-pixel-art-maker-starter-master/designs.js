e105e42c2bad023776978dcbde65a9ff1705f663
Galiant <antonijodev@gmail.com>
2017-12-16 13:01:07 +0000

    makeGrid function

--- project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/designs.js
@@ -4,7 +4,36 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+  // Your code goes here!
 
-// Your code goes here!
+  const submit = $("#submit");
 
+  // Get number of grid's height end width
+  var gridHeight = $("input_height").val();
+  var gridWidth = $("#input_width").val();
+
+  // Get canvas
+  var canvas = $("pixel_canvas");
+
+  // Reset to empty canvas
+  canvas.children().remove();
+
+  // Create grid's height
+  for (var i = 0; i < gridHeight; i++) {
+    canvas.append("<tr></tr>");
+    // Create grid's width
+    for (var j = 0; j < gridWidth; j++) {
+      canvas
+        .children()
+        .last()
+        .append("<td></td>");
+    }
+  }
+  // Default background color of canvas
+  $("td").css("backgroundColor", "#FFF");
 }
+
+// Event listeners
+$(submit).click(function() {
+  makeGrid();
+});
