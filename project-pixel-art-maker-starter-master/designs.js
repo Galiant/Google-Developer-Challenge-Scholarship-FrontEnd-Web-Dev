28c903cd58d514656952e7271015715c6fb4a436
Galiant <antonijodev@gmail.com>
2017-12-17 15:52:18 +0000

    Grid is here!!!!

--- project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/designs.js
@@ -1,39 +1,27 @@
-// Select color input
-// Select size input
+$(function() {
+  // When size is submitted by the user, call makeGrid()
+  function makeGrid(gridHeight, gridWidth) {
+    var table = $("#pixel_canvas");
+    var color = $("#colorPicker");
 
-// When size is submitted by the user, call makeGrid()
+    // Get number of grid's height end width
+    var gridHeight = $("#input_height").val();
+    var gridWidth = $("#input_width").val();
 
-function makeGrid() {
-  // Your code goes here!
-
-  const submit = $("#submit");
-
-  // Get number of grid's height end width
-  var gridHeight = $("input_height").val();
-  var gridWidth = $("#input_width").val();
-
-  // Get canvas
-  var canvas = $("pixel_canvas");
-
-  // Reset to empty canvas
-  canvas.children().remove();
-
-  // Create grid's height
-  for (var i = 0; i < gridHeight; i++) {
-    canvas.append("<tr></tr>");
-    // Create grid's width
-    for (var j = 0; j < gridWidth; j++) {
-      canvas
-        .children()
-        .last()
-        .append("<td></td>");
+    // Create grid's height
+    for (var i = 0; i < gridHeight; i++) {
+      table.append("<tr></tr>");
+      // Create grid's width
+      for (var j = 0; j < gridWidth; j++) {
+        $("tr")
+          .last()
+          .append("<td></td>");
+      }
     }
   }
-  // Default background color of canvas
-  $("td").css("backgroundColor", "#FFF");
-}
-
-// Event listeners
-$(submit).click(function() {
-  makeGrid();
+  // Event listeners
+  $("#submit").click(function(event) {
+    makeGrid();
+    event.preventDefault();
+  });
 });
